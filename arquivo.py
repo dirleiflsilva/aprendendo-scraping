b86bb57ecff10c5ebe8afa2a319527791a11f924
Dirlei Friedrich <dirleiflsilva@gmail.com>
2020-05-12 22:59:44 -0300

    revisando e documentando o exercício.

--- arquivo.py
+++ arquivo.py
@@ -1,17 +1,31 @@
-''' escrevendo em um arquivo
+"""
+Manipulação de arquivos
 
-arq = open('arquivo.txt', 'w')
-arq.write('Python eh legal\n')
-arq.write('curso de BS4')
+escrevendo em um arquivo
+"""
+'''
+arq = open('arquivo.txt', 'w') # w para escrever
+arq.write('Python é legal\n') # \n = quebra de linha
+arq.write('curso de BS4\n')
+arq.write('Estou aprendendo a programar em Python!')
 arq.close()
 '''
 
-''' lendo um arquivo
-arq = open('arquivo.txt','r')
+"""
+Manipulação de arquivos
+
+lendo um arquivo
+"""
+'''
+arq = open('arquivo.txt','r') # r para ler
 print(arq.read())
 arq.close()
 '''
 
-# forma pythonica de ler arquivo
-with open('arquivo.txt','r') as f:
-    print(f.read())
+"""
+Manipulação de arquivos
+
+forma pythonica de ler arquivo
+"""
+with open('arquivo.txt','r') as file:
+    print(file.read())
