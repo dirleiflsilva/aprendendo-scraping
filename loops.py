2475b82a0062cd94d57f1aa98d02665a49237170
Dirlei Friedrich <dirleiflsilva@gmail.com>
2020-05-21 23:12:52 -0300

    revisando e documentando os exercícios

--- loops.py
+++ loops.py
@@ -1,10 +1,22 @@
+"""
+loop utilizando while
+"""
 '''
+# inicializa contador
 cont = 1
 
 while cont <= 10:
-    print('Não vou mais entrar no WhatsApp')
+    print('Estou aprendendo Python com BeautifulSoup!')
     cont = cont + 1
 '''
 
-for i in range(1, 10, 2): #passo 2
-    print('Não vou mais entrar no WhatsApp')
+"""
+loop utilizando for
+"""
+# range(1, 10) inicia em 1 e incrementa em 1 até 10 - 1 = 9
+# for cont in range(1, 10):
+
+# range(1, 10, 2) neste caso inicia em 1 e incrementa em 2, ou seja, 1, 3, 5, 7, 9
+for _ in range(1, 10, 2):
+    print('Estou aprendendo Python com BeautifulSoup!')
+
--- webscraping01.py
+++ webscraping01.py
@@ -1,18 +1,30 @@
+"""
+parseando arquivo html
+"""
+# importando modulo requests
 import requests
+# importando modulo BeautifulSoup do pacote bs4
 from bs4 import BeautifulSoup
 
 '''
-with open('arquivo01.html','r') as f:
-    soup_string = BeautifulSoup(f.read(),'html.parser') # parser padrão do bs4
+# usando with para garantir a finalização do processo/recurso
+with open('arquivo01.html','r') as file: # r = read
+    soup_string = BeautifulSoup(file.read(),'html.parser') # parser padrão do bs4
 print(soup_string)
 '''
 
+"""
+utilizando parser lxml em uma página web
+"""
 '''
 r = requests.get('http://www.pudim.com.br')
-soup_string = BeautifulSoup(r.text,'lxml') # parser lxml
+soup_string = BeautifulSoup(r.text,'lxml')
 print(soup_string)
 '''
 
+"""
+utilizando parser html5lib
+"""
 with open('arquivo01.html','r') as f:
-    soup_string = BeautifulSoup(f.read(),'html5lib') # parser html5lib
+    soup_string = BeautifulSoup(f.read(),'html5lib')
 print(soup_string)
