c71ed7c1f9da56f459a539a35cbc8c095eadb8e3
Dirlei Friedrich <dirleiflsilva@gmail.com>
2020-05-26 22:24:54 -0300

    revisando e documentando os exercícios

--- webscraping02.py
+++ webscraping02.py
@@ -1,13 +1,36 @@
+"""
+parseando arquivo html / tag
+parser lxml
+"""
+# importando modulo BeautifulSoup do pacote bs4
 from bs4 import BeautifulSoup
 
 # abrir arquivo para leitura
 with open('arquivo01.html','r') as f:
     soup = BeautifulSoup(f,'lxml')
 
+# busca primeira ocorrencia title
 tag = soup.title
 print(tag)
-print(tag.name)
+print(tag.name) # nome da tag = title
 
 tag = soup.p
 print(tag)
 print(tag.name)
+
+'''
+saída das tags
+### title
+<title>
+    The Dormouse's story
+   </title>
+title
+
+### p
+<p class="title">
+<b>
+     The Dormouse's story
+   </b>
+</p>
+p
+'''
